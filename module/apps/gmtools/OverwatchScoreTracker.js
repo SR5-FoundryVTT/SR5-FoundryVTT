b62c6612ddf0382fd7a4207e6684643fa0065337
Shawn Milligan <smilligan93@gmail.com>
2020-06-01 13:36:06 -0400

    add logic for overwatch-score-tracker

--- module/apps/gmtools/OverwatchScoreTracker.js
+++ module/apps/gmtools/OverwatchScoreTracker.js
@@ -7,7 +7,8 @@ export class OverwatchScoreTracker extends Application {
         options.id = 'overwatch-score-tracker';
         options.classes = ['sr5'];
         options.title = game.i18n.localize('SR5.OverwatchScoreTrackerTitle');
-        options.template = 'systems/shadowrun5e/templates/apps/gmtools/overwatch-score-tracker.html';
+        options.template =
+            'systems/shadowrun5e/templates/apps/gmtools/overwatch-score-tracker.html';
         options.width = 600;
         options.height = 'auto';
         return options;
@@ -19,30 +20,65 @@ export class OverwatchScoreTracker extends Application {
                 acc.push(user.character.data);
             }
             return acc;
-        }, [])
-
-        console.log(actors);
+        }, []);
 
         return {
             actors,
-        }
+        };
     }
 
     activateListeners(html) {
-        console.log(html);
-
         html.find('.overwatch-score-reset').on('click', this._resetOverwatchScore.bind(this));
+        html.find('.overwatch-score-add').on('click', this._addOverwatchScore.bind(this));
+        html.find('.overwatch-score-input').on('change', this._setOverwatchScore.bind(this));
+        html.find('.overwatch-score-roll-15-minutes').on(
+            'click',
+            this._rollFor15Minutes.bind(this)
+        );
     }
 
     _getActorFromEvent(event) {
         const id = event.currentTarget.closest('.item').dataset.actorId;
-        if (id) return game.actors.find(a => a._id === id);
+        if (id) return game.actors.find((a) => a._id === id);
+    }
+
+    _setOverwatchScore(event) {
+        const actor = this._getActorFromEvent(event);
+        const amount = event.currentTarget.value;
+        if (amount && actor) {
+            actor.setOverwatchScore(amount).then(() => this.render());
+        }
+    }
+
+    _addOverwatchScore(event) {
+        const actor = this._getActorFromEvent(event);
+        const amount = parseInt(event.currentTarget.dataset.amount);
+        if (amount && actor) {
+            const os = actor.getOverwatchScore();
+            actor.setOverwatchScore(os + amount).then(() => this.render());
+        }
     }
 
     _resetOverwatchScore(event) {
         event.preventDefault();
         const actor = this._getActorFromEvent(event);
-        console.log(actor);
-        console.log('reset ');
+        if (actor) {
+            actor.setOverwatchScore(0).then(() => this.render());
+        }
+    }
+
+    _rollFor15Minutes(event) {
+        event.preventDefault();
+        const actor = this._getActorFromEvent(event);
+        if (actor) {
+            const roll = new Roll('2d6');
+            roll.toMessage({
+                rollMode: 'gmroll',
+            });
+            if (roll.total) {
+                const os = actor.getOverwatchScore();
+                actor.setOverwatchScore(os + roll.total).then(() => this.render());
+            }
+        }
     }
 }
