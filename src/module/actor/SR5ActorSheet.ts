7b08a6d08bd6f9cbee4fa9c299fb7a19c538b643
taMiF <jan.schoska@gmail.com>
2020-12-29 13:26:08 +0100

    Issue spirit calculation errors (#206)
    * Misc. initiative modifiers weren't applied to Spirits
    * Removed character type actor fields from other actors types.
    * Fixed Spirit of Man attribute / initiative calculation
    * Automatic build of bundle

--- src/module/actor/SR5ActorSheet.ts
+++ src/module/actor/SR5ActorSheet.ts
@@ -130,8 +130,8 @@ export class SR5ActorSheet extends ActorSheet {
     }
 
     _prepareActorTypeFields(data: SR5ActorSheetData) {
-        data.isCharacter = this.actor.data.type === 'character';
-        data.isSpirit = this.actor.data.type === 'spirit';
+        data.isCharacter = this.actor.isCharacter();
+        data.isSpirit = this.actor.isSpirit();
     }
 
     _prepareMatrixAttributes(data) {
