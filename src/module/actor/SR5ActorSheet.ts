a00620a779a9e991e40a216c0cda2220ceac584b
Jan Schoska <jan.schoska@gmail.com>
2021-02-13 17:45:52 +0100

    Perception skill was used for piloting. Any none autopilot tests don't work though, as attributes can't be found (no driver). closes #171

--- src/module/actor/SR5ActorSheet.ts
+++ src/module/actor/SR5ActorSheet.ts
@@ -555,10 +555,10 @@ export class SR5ActorSheet extends ActorSheet {
                         await this.actor.rollDronePerception(options);
                         break;
                     case 'infiltration':
-                        this.actor.rollDroneInfiltration(options);
+                        await this.actor.rollDroneInfiltration(options);
                         break;
                     case 'pilot-vehicle':
-                        this.actor.rollPilotVehicle(options);
+                        await this.actor.rollPilotVehicle(options);
                         break;
                 }
                 break;
@@ -567,7 +567,7 @@ export class SR5ActorSheet extends ActorSheet {
             case 'attribute':
                 const attribute = split[1];
                 if (attribute) {
-                    this.actor.rollAttribute(attribute, options);
+                    await this.actor.rollAttribute(attribute, options);
                 }
                 break;
             // end attribute
