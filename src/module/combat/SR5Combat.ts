74119f4aba126943253453ef49b041dcf76b0da9
Jan Schoska <jan.schoska@gmail.com>
2021-03-21 19:05:47 +0100

    Cleaned up Combat handling and always show zero score combatants.

--- src/module/combat/SR5Combat.ts
+++ src/module/combat/SR5Combat.ts
@@ -1,7 +1,13 @@
 import SR5CombatData = Shadowrun.SR5CombatData;
 import {SR5Actor} from "../actor/SR5Actor";
+import {CombatRules} from "../sr5/Combat";
 
+/**
+ * Foundry combat implementation for Shadowrun5 rules.
+ */
 export class SR5Combat extends Combat {
+    data: SR5CombatData;
+
     get initiativePass(): number {
         return this.data?.initiativePass || 0;
     }
@@ -10,20 +16,21 @@ export class SR5Combat extends Combat {
         // @ts-ignore
         super(...args);
 
-        Hooks.on('updateActor', (actor) => {
-            const combatant = this.getActorCombatant(actor);
-            if (combatant) {
-                // TODO handle monitoring Wound changes
-            }
-        });
+        // NOTE: This is currently handled during damage application in the Actor.
+        // Hooks.on('updateActor', (actor) => {
+        //     const combatant = this.getActorCombatant(actor);
+        //     if (combatant) {
+        //         // handle monitoring Wound changes
+        //     }
+        // });
     }
 
-    /** Use the given actors token to get the combatant.
+    /**
+     * Use the given actors token to get the combatant.
      * NOTE: The token must be used, instead of just the actor, as unlinked tokens will all use the same actor id.
      */
     getActorCombatant(actor: SR5Actor): undefined | any {
         const token = actor.getToken();
-        // Sidebar actors won't have a token to connect a combatant to.
         if (!token) return;
         return this.getCombatantByToken(token.id);
     }
@@ -74,8 +81,6 @@ export class SR5Combat extends Combat {
         );
         return options;
     }
-
-    data: SR5CombatData;
 
     protected _onUpdate(data: object, ...args) {
         console.log(data);
@@ -134,99 +139,129 @@ export class SR5Combat extends Combat {
     }
 
     /**
-     * @Override
+     * Return an array of combatants sorted by Shadowrun initiative order and maybe remove combatants with a zero ini.
      * remove any turns that are less than 0
-     * filter using ERIC
+     * @Override
      */
     setupTurns(): any[] {
-        const turns = super.setupTurns().filter((turn) => {
-            if (turn.initiative === null) return true;
-
-            const init = Number(turn.initiative);
-            if (isNaN(init)) return true;
-            return init > 0;
-        });
-        // @ts-ignore
-        this.turns = turns.sort(SR5Combat.sortByRERIC);
-        return turns;
+        return super.setupTurns();
+        // const turns = super.setupTurns().filter((turn) => {
+        //     if (turn.initiative === null) return true;
+        //
+        //     const init = Number(turn.initiative);
+        //     if (isNaN(init)) return true;
+        //     return init > 0;
+        // });
+        // // @ts-ignore
+        // this.turns = turns.sort(SR5Combat.sortByRERIC);
+        // return turns;
     }
 
     /**
-     * @Override
-     * proceed to the next turn
-     * - handles going to next initiative pass or combat round.
+     * Return the position in the current ini pass of the next undefeated combatant.
      */
-    async nextTurn(): Promise<void> {
-        let turn = this.turn;
-        let skip = this.settings.skipDefeated;
-        // Determine the next turn number
-        let next: number | null = null;
-        if (skip) {
-            for (let [i, t] of this.turns.entries()) {
-                if (i <= turn) continue;
-                // @ts-ignore
-                if (!t.defeated) {
-                    next = i;
-                    break;
-                }
-            }
-        } else next = turn + 1;
-
-        // Maybe advance to the next round/init pass
-        let round = this.round;
-        let initPass = this.initiativePass;
-
-        // if both are 0, we just started so set both to 1
-        if (round === 0 && initPass === 0) {
-            initPass = initPass + 1;
-            round = round + 1;
-            next = 0;
-        } else if (next === null || next >= this.turns.length) {
-            const combatants: any[] = [];
-
-            // check for initpass
-            const over10Init = this.combatants.reduce((accumulator, running) => {
-                return accumulator || Number(running.initiative) > 10;
-            }, false);
-
-            // do an initiative pass
-            if (over10Init) {
-                next = 0;
-                initPass = initPass + 1;
-                // adjust combatants
-                for (const c of this.combatants) {
-                    let init = Number(c.initiative);
-                    init -= 10;
-                    // @ts-ignore
-                    combatants.push({ _id: c._id, initiative: init });
-                }
-            } else {
-                next = 0;
-                round = round + 1;
-                initPass = 0;
-                // resetall isn't typed
-                // @ts-ignore
-                await this.resetAll();
-                await this.rollAll();
-            }
-
-            if (combatants.length > 0) {
-                // @ts-ignore
-                await this.updateCombatant(combatants);
-            }
-
-            if (skip) {
-                // @ts-ignore
-                next = this.turns.findIndex((t) => !t.defeated);
-                if (next === -1) {
-                    // @ts-ignore
-                    ui.notifications.warn(game.i18n.localize('COMBAT.NoneRemaining'));
-                    next = 0;
-                }
+    get nextUndefeatedTurnPosition(): number {
+        for (let [turnInPass, combatant] of this.turns.entries()) {
+            // Skipping is only interesting when moving forward.
+            if (turnInPass <= this.turn) continue;
+            // @ts-ignore
+            if (!combatant.defeated) {
+                return turnInPass;
             }
         }
+        // The current turn is the last undefeated combatant. So go to the end and beeeeyooond.
+        return this.turns.length;
+    }
 
-        // Update the encounter
-        await this.update({ round: round, turn: next, initiativePass: initPass });
+    /**
+     * Determine wheter the current combat situation (current turn order) needs and can have an initiative pass applied.
+     * @return true means that an initiative pass must be applied
+     */
+    doIniPass(nextTurn: number): boolean {
+        // We're currently only stepping from combatant to combatant.
+        if (nextTurn < this.turns.length) return false;
+        // Prepare another possible initiative order.
+        const currentScores = this.combatants.map(combatant => Number(combatant.initiative));
+
+        return CombatRules.iniOrderCanDoAnotherPass(currentScores);
+    }
+
+    /**
+     * After all combatants have had their action phase (click on next 'turn') handle shadowrun rules for
+     * initiative pass and combat turn.
+     *
+     * As long as a combatant still has a positive initiative score left, go to the next pass.
+     *  Raise the Foundry turn and don't raise the Foundry round.
+     * As soons as all combatants have no initiative score left, go to the next combat round.
+     *  Reset the Foundry pass and don't raise the Foundry turn.
+     *
+     * Retrigger Initiative Rolls on each new Foundry round.
+     *
+     * Handle these cases:
+     * 1. New Combat! New Start of all things! Roll initiative.
+     * 2. Current Combat. Go to the next Foundry turn / the next combatant
+     *
+     * * @Override
+     */
+    async nextTurn(): Promise<void> {
+        // Maybe advance to the next round/init pass
+        let nextRound = this.round;
+        let initiativePass = this.initiativePass;
+        // Get the next viable turn position.
+        let nextTurn = this.settings.skipDefeated ?
+            this.nextUndefeatedTurnPosition :
+            this.turn + 1;
+
+        // Start of the combat Handling
+        if (nextRound === 0 && initiativePass === 0) {
+            nextRound = 1;
+            initiativePass = 1;
+            nextTurn = 0;
+
+            await this.update({ round: nextRound, turn: nextTurn, initiativePass });
+            return;
+        }
+
+        // Just step from one combatant to the next!
+        if (nextTurn < this.turns.length) {
+            await this.update({ round: nextRound, turn: nextTurn, initiativePass });
+            return;
+        }
+
+        // Initiative Pass Handling.
+        if (this.doIniPass(nextTurn)) {
+            initiativePass = initiativePass + 1;
+            // Start at the top!
+            nextTurn = 0;
+
+            const combatants: any[] = [];
+            for (const combatant of this.combatants) {
+                const initiative = CombatRules.reduceIniResultAfterPass(Number(combatant.initiative));
+                combatants.push({_id: combatant._id, initiative});
+            }
+            if (combatants.length > 0)
+                // @ts-ignore
+                await this.updateCombatant(combatants);
+
+             await this.update({ round: nextRound, turn: nextTurn, initiativePass });
+             return;
+        }
+
+        // Initiative Round Handling.
+        // NOTE: It's not checked if the next is needed. This should result int he user noticing the turn going up, when it
+        //       maybe shouldn't and reporting a unhandled combat phase flow case.
+        return this.nextRound();
+    }
+
+    async nextRound(): Promise<void> {
+        const nextRound = this.round + 1;
+        const initiativePass = 0;
+        // Start at the top!
+        const nextTurn = 0;
+
+        await this.resetAll();
+        await this.rollAll();
+
+        await this.update({ round: nextRound, turn: nextTurn, initiativePass });
     }
 }
