35efec96176a9e72321ff9f5f4ec8d30d50ca7d9
Jan Schoska <jan.schoska@gmail.com>
2021-03-27 09:32:07 +0100

    Rtyped macros

--- src/module/macros.ts
+++ src/module/macros.ts
@@ -6,6 +6,8 @@
  * @returns {Promise}
  */
 export async function createItemMacro(item, slot) {
+    if (!game || !game.macros) return;
+
     const command = `game.shadowrun5e.rollItemMacro("${item.name}");`;
     let macro = game.macros.entities.find((m) => m.name === item.name);
     if (!macro) {
@@ -20,6 +22,7 @@ export async function createItemMacro(item, slot) {
             { displaySheet: false },
         )) as Macro;
     }
+
     if (macro) game.user?.assignHotbarMacro(macro, slot);
 }
 
@@ -30,13 +33,15 @@ export async function createItemMacro(item, slot) {
  * @return {Promise}
  */
 export function rollItemMacro(itemName) {
+    if (!game || !game.actors) return;
+
     const speaker = ChatMessage.getSpeaker();
     let actor;
     if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!speaker.actor) return;
     if (!actor) actor = game.actors.get(speaker.actor);
     const item = actor ? actor.items.find((i) => i.name === itemName) : null;
     if (!item) {
-        // @ts-ignore
         return ui.notifications?.warn(`Your controlled Actor does not have an item named ${itemName}`);
     }
 
