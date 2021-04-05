921dbcb491825af1aac4fd4a805e4cbca3ac0e92
Jan Schoska <jan.schoska@gmail.com>
2021-04-05 11:44:44 +0200

    Added a few basic SR5Item integration tests

--- src/test/quench.ts
+++ src/test/quench.ts
@@ -1,5 +1,7 @@
 import {shadowrunRulesModifiers} from "./sr5.Modifiers.spec";
+import {shadowrunSR5Item} from "./sr5.SR5Item.spec";
 
 export const quenchRegister = quench => {
     quench.registerBatch("shadowrun5e.rules.modifiers", shadowrunRulesModifiers);
+    quench.registerBatch("shadowrun5e.entities.items", shadowrunSR5Item);
 };
