ab0c66406391706bf58facc6e06f8d9bf68284b4
Jan Schoska <jan.schoska@gmail.com>
2021-04-06 10:56:35 +0200

    Calculate the IC matrix condition monitor

--- src/module/actor/prep/ICPrep.ts
+++ src/module/actor/prep/ICPrep.ts
@@ -2,19 +2,30 @@ import {BaseActorPrep} from "./BaseActorPrep";
 import {ModifiersPrep} from "./functions/ModifiersPrep";
 import SR5ICType = Shadowrun.SR5ICType;
 import ICActorData = Shadowrun.ICActorData;
+import {MatrixRules} from "../../sr5/Matrix";
 
 export class ICPrep extends BaseActorPrep<SR5ICType, ICActorData> {
     prepare() {
         ICPrep.prepareModifiers(this.data);
+        ICPrep.prepareMatrix(this.data);
+        ICPrep.prepareMatrixTrack(this.data);
     }
 
     /**
      * Misc Tab IC modifiers.
      * @param data
      */
-    static prepareModifiers(data: ICActorData) {
+    static prepareModifiers(data) {
         let modifiers = ModifiersPrep.commonModifiers;
         modifiers = modifiers.concat(ModifiersPrep.matrixModifiers);
         ModifiersPrep.setupModifiers(data, modifiers);
+    }
+
+    static prepareMatrix(data) {
+        data.matrix.rating = MatrixRules.getICDeviceRating(data.host.rating);
+    }
+
+    static prepareMatrixTrack(data) {
+        data.matrix.condition_monitor.max = MatrixRules.getConditionMonitor(data.matrix.rating);
     }
 }
---
+++ src/module/sr5/Matrix.ts
@@ -0,0 +1,23 @@
+export class MatrixRules {
+    /**
+     * Calculate the matrix condition monitor based on SR5#228 'Matrix Damage'
+     *
+     * The result is round up as for physical and stun monitor (SR5#101), even though it's not specified for
+     * matrix monitors specifically.
+     *
+     * @param deviceRating The device rating of the matrix device for the condition monitor.
+     * @return The condition max condition monitor value
+     */
+    static getConditionMonitor(deviceRating: number): number {
+        return Math.ceil(8 + (deviceRating / 2));
+    }
+
+    /**
+     * Derive the IC device rating based of it's hosts rating based on SR5#247 'Intrusion Countermeasures'
+     *
+     * @param hostRating
+     */
+    static getICDeviceRating(hostRating: number): number {
+        return hostRating;
+    }
+}
