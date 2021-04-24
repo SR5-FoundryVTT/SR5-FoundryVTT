68697cda5a77dce0f49a59ec2c7e2be00e041da3
Jan Schoska <jan.schoska@gmail.com>
2021-04-24 18:46:43 +0200

    Added i18n labels for effect management

--- src/module/effects.ts
+++ src/module/effects.ts
@@ -12,13 +12,15 @@
  */
 export function onManageActiveEffect(event, owner) {
   event.preventDefault();
+  // These element grabs rely heavily on HTML structure within the templates.
   const icon = event.currentTarget;
   const item = event.currentTarget.closest('.list-item');
   const effect = item.dataset.itemId ? owner.effects.get(item.dataset.itemId) : null;
+  // The HTML dataset must be defined
   switch ( icon.dataset.action ) {
     case "create":
       return ActiveEffect.create({
-        label: "New Effect",
+        label: game.i18n.localize("SR5.ActiveEffect.New"),
         icon: "icons/svg/aura.svg",
         origin: owner.uuid,
         "duration.rounds": item.dataset.effectType === "temporary" ? 1 : undefined,
@@ -30,6 +32,9 @@ export function onManageActiveEffect(event, owner) {
       return effect.delete();
     case "toggle":
       return effect.update({disabled: !effect.data.disabled});
+    default:
+        console.error(`An active effect with the id '${effect}' couldn't be managed as no action has been defined within the template.`);
+      return;
   }
 }
 
@@ -44,17 +49,17 @@ export function prepareActiveEffectCategories(effects) {
     const categories = {
       temporary: {
         type: "temporary",
-        label: "Temporary Effects",
+        label: game.i18n.localize("SR5.ActiveEffect.Types.Temporary"),
         effects: []
       },
       passive: {
         type: "passive",
-        label: "Passive Effects",
+        label: game.i18n.localize("SR5.ActiveEffect.Types.Passive"),
         effects: []
       },
       inactive: {
         type: "inactive",
-        label: "Inactive Effects",
+        label: game.i18n.localize("SR5.ActiveEffect.Types.Inactive"),
         effects: []
       }
     };
