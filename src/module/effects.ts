01ab3efa71b26ded20e0db725e25552fd364fdef
Jan Schoska <jan.schoska@gmail.com>
2021-06-11 16:15:36 +0200

    Migrated effects to 0.8

--- src/module/effects.ts
+++ src/module/effects.ts
@@ -3,39 +3,42 @@
  * All functions have been taken from : https://gitlab.com/foundrynet/dnd5e/-/blob/master/module/effects.js
  *
  * There have been some alterations made to fit the Shadowrun5e system.
-  */
+ */
+
+import {SR5Actor} from "./actor/SR5Actor";
+import {SR5Item} from "./item/SR5Item";
 
 /**
  * Manage Active Effect instances through the Actor Sheet via effect control buttons.
  * @param {MouseEvent} event      The left-click event on the effect control
  * @param {Actor|Item} owner      The owning entity which manages this effect
  */
-export function onManageActiveEffect(event, owner) {
-  event.preventDefault();
-  // These element grabs rely heavily on HTML structure within the templates.
-  const icon = event.currentTarget;
-  const item = event.currentTarget.closest('.list-item');
-  const effect = item.dataset.itemId ? owner.effects.get(item.dataset.itemId) : null;
-  // The HTML dataset must be defined
-  switch ( icon.dataset.action ) {
-    case "create":
-      return ActiveEffect.create({
-        label: game.i18n.localize("SR5.ActiveEffect.New"),
-        icon: "icons/svg/aura.svg",
-        origin: owner.uuid,
-        "duration.rounds": item.dataset.effectType === "temporary" ? 1 : undefined,
-        disabled: item.dataset.effectType === "inactive"
-      }, owner).create();
-    case "edit":
-      return effect.sheet.render(true);
-    case "delete":
-      return effect.delete();
-    case "toggle":
-      return effect.update({disabled: !effect.data.disabled});
-    default:
-        console.error(`An active effect with the id '${effect}' couldn't be managed as no action has been defined within the template.`);
-      return;
-  }
+export function onManageActiveEffect(event, owner: SR5Actor|SR5Item) {
+    event.preventDefault();
+    // These element grabs rely heavily on HTML structure within the templates.
+    const icon = event.currentTarget;
+    const item = event.currentTarget.closest('.list-item');
+    const effect = item.dataset.itemId ? owner.effects.get(item.dataset.itemId) : null;
+    // The HTML dataset must be defined
+    switch (icon.dataset.action) {
+        case "create":
+            return owner.createEmbeddedDocuments('ActiveEffect', [{
+                label: game.i18n.localize("SR5.ActiveEffect.New"),
+                icon: "icons/svg/aura.svg",
+                origin: owner.uuid,
+                "duration.rounds": item.dataset.effectType === "temporary" ? 1 : undefined,
+                disabled: item.dataset.effectType === "inactive"
+            }]);
+        case "edit":
+            return effect.sheet.render(true);
+        case "delete":
+            return effect.delete();
+        case "toggle":
+            return effect.update({disabled: !effect.data.disabled});
+        default:
+            console.error(`An active effect with the id '${effect}' couldn't be managed as no action has been defined within the template.`);
+            return;
+    }
 }
 
 /**
@@ -47,29 +50,29 @@ export function prepareActiveEffectCategories(effects) {
 
     // Define effect header categories
     const categories = {
-      temporary: {
-        type: "temporary",
-        label: game.i18n.localize("SR5.ActiveEffect.Types.Temporary"),
-        effects: []
-      },
-      passive: {
-        type: "passive",
-        label: game.i18n.localize("SR5.ActiveEffect.Types.Passive"),
-        effects: []
-      },
-      inactive: {
-        type: "inactive",
-        label: game.i18n.localize("SR5.ActiveEffect.Types.Inactive"),
-        effects: []
-      }
+        temporary: {
+            type: "temporary",
+            label: game.i18n.localize("SR5.ActiveEffect.Types.Temporary"),
+            effects: []
+        },
+        passive: {
+            type: "passive",
+            label: game.i18n.localize("SR5.ActiveEffect.Types.Passive"),
+            effects: []
+        },
+        inactive: {
+            type: "inactive",
+            label: game.i18n.localize("SR5.ActiveEffect.Types.Inactive"),
+            effects: []
+        }
     };
 
     // Iterate over active effects, classifying them into categories
-    for ( let effect of effects ) {
-      effect._getSourceName(); // Trigger a lookup for the source name
-      if ( effect.data.disabled ) categories.inactive.effects.push(effect);
-      else if ( effect.isTemporary ) categories.temporary.effects.push(effect);
-      else categories.passive.effects.push(effect);
+    for (let effect of effects) {
+        effect._getSourceName(); // Trigger a lookup for the source name
+        if (effect.data.disabled) categories.inactive.effects.push(effect);
+        else if (effect.isTemporary) categories.temporary.effects.push(effect);
+        else categories.passive.effects.push(effect);
     }
     return categories;
 }
