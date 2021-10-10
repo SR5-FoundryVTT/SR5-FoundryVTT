673e2ef83ddb0ecc13cc11e46600adc314eaff7e
Jan Schoska <jan.schoska@gmail.com>
2021-10-10 20:44:01 +0200

    Setting for token health bars. close #491

--- src/module/token/SR5Token.ts
+++ src/module/token/SR5Token.ts
@@ -1,11 +1,14 @@
+import {FLAGS, SYSTEM_NAME} from "../constants";
+
 export class SR5Token extends Token {
     override _drawBar(number: number, bar: PIXI.Graphics, data: ReturnType<Token['getBarAttribute']>){
+        const tokenHealthBars = game.settings.get(SYSTEM_NAME, FLAGS.TokenHealthBars);
         // FoundryVTT draws resource bars as full/good when the value is the
         // same as the max and empty/bad at 0 (colored along a gradient).
         // Shadowrun condition trackers count up from 0 to the maximum.
         // We flip the values from Shadowrun format to FoundryVTT format here
         // for drawing.
-        if (data.attribute.startsWith('track')) {
+        if (tokenHealthBars && data.attribute.startsWith('track')) {
             data.value = data.max - data.value;
         }
         super._drawBar(number, bar, data);
