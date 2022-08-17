ff23d7227e9fefdd4de12afd91c65388d36ad976
Jan Schoska <jan.schoska@gmail.com>
2022-08-17 21:34:05 +0200

    Skill macros are configured by the skill label but where fetched by id.

--- src/module/types/rolls/ActorRolls.ts
+++ src/module/types/rolls/ActorRolls.ts
@@ -54,7 +54,10 @@ declare namespace Shadowrun {
     };
 
     type SkillRollOptions = ActorRollOptions & {
+        // A specialization should be used.
         specialization?: boolean
+        // Skills should be searched not by id but their label.
+        byLabel?: boolean
     };
 
     type SkillDialogOptions = {
