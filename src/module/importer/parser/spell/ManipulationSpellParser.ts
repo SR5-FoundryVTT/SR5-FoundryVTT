cabb451e15d23c402f4804cc3f14a663b346da75
Jan Schoska <jan.schoska@gmail.com>
2023-01-22 09:15:17 +0100

    Added Chummer5 2.55 support. closes #676

--- src/module/importer/parser/spell/ManipulationSpellParser.ts
+++ src/module/importer/parser/spell/ManipulationSpellParser.ts
@@ -6,11 +6,12 @@ export class ManipulationSpellParser extends SpellParserBase {
     Parse(jsonData: object, item: SpellItemData, jsonTranslation?: object): SpellItemData {
         item = super.Parse(jsonData, item, jsonTranslation);
 
-        let descriptor = ImportHelper.StringValue(jsonData, 'descriptor');
         // A few spells have a missing descriptor instead of an empty string.
         // The field is <descriptor /> rather than <descriptor></descriptor>
         // which gets imported as undefined rather than empty string (sigh)
         // Rather than refactor our ImportHelper we'll handle it in here.
+        // Sometimes the field misses altogether.
+        let descriptor = ImportHelper.StringValue(jsonData, 'descriptor', '');
         if (descriptor === undefined) {
             descriptor = '';
         }
