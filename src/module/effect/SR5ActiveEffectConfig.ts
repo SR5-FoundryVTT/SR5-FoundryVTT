00fa76a649881adab544d7f363bd9916ebaed28a
Jan Schoska <jan.schoska@gmail.com>
2023-07-08 15:48:39 +0200

    Restructured for readability

--- src/module/effect/SR5ActiveEffectConfig.ts
+++ src/module/effect/SR5ActiveEffectConfig.ts
@@ -1,3 +1,9 @@
+/**
+ * Shadowrun system alteres some behaviours of Active Effects, making a custom ActiveEffectConfig necessary.
+ * 
+ * NOTE: A ActiveEffectConfig class is comparable to a DocumentSheet class, but Foundry differentiates between
+ * 'Config' and 'Sheet'.
+ */
 export class SR5ActiveEffectConfig extends ActiveEffectConfig {
     override get template(): string {
         return 'systems/shadowrun5e/dist/templates/effect/active-effect-config.html';
@@ -5,7 +11,22 @@ export class SR5ActiveEffectConfig extends ActiveEffectConfig {
 
     override getData(options?: Application.RenderOptions): Promise<ActiveEffectConfig.Data> | ActiveEffectConfig.Data {
         const data = super.getData(options) as any;
-        data.modes = {...data.modes, 0: game.i18n.localize('SR5.ActiveEffect.Modes.Modify')};
+        
+        data.modes = this.injectModifyLabelOverCustomMode(data.modes);
+
         return data;
+    }
+
+    /**
+     * Foundry provides a custom mode for systems to implement behaviour with.
+     * 
+     * Shadowrun uses this mode to implement 'modify' mode, with complex behaviour.
+     * To give users better information about the mode, inject a 'modify' label.
+     * 
+     * @param modes A object prepared for display using Foundry select handlebarjs helper.
+     * @returns Copy of the original modes and labels.
+     */
+    injectModifyLabelOverCustomMode(modes: Record<number, string>): Record<number, string> {
+        return {...modes, 0: game.i18n.localize('SR5.ActiveEffect.Modes.Modify')};
     }
 }
