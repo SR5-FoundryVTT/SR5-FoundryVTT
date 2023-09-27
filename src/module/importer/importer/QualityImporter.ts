fdd19d1ebf339361d86d874b890686c7bf8cf0a5
ian612 <34078802+ian612@users.noreply.github.com>
2023-09-27 10:36:55 -0400

    quality importer fixed

--- src/module/importer/importer/QualityImporter.ts
+++ src/module/importer/importer/QualityImporter.ts
@@ -3,6 +3,7 @@ import { ImportHelper } from '../helper/ImportHelper';
 import { QualityParserBase } from '../parser/quality/QualityParserBase';
 import QualityItemData = Shadowrun.QualityItemData;
 import {Helpers} from "../../helpers";
+import { SR5 } from "../../config";
 
 export class QualityImporter extends DataImporter<QualityItemData, Shadowrun.QualityData> {
     public override categoryTranslations: any;
@@ -34,21 +35,35 @@ export class QualityImporter extends DataImporter<QualityItemData, Shadowrun.Qua
         for (let i = 0; i < jsonDatas.length; i++) {
             let jsonData = jsonDatas[i];
 
+            // Check to ensure the data entry is supported and the correct category
             if (DataImporter.unsupportedEntry(jsonData)) {
                 continue;
             }
 
+            // Create the item
             let item = parser.Parse(jsonData, this.GetDefaultData({type: 'quality'}), this.itemTranslations);
-
-            let category = ImportHelper.StringValue(jsonData, 'category');
+            let category = ImportHelper.StringValue(jsonData, 'category').toLowerCase();
             //@ts-ignore TODO: Foundry Where is my foundry base data?
-            item.folder = folders[category.toLowerCase()].id;
+            item.folder = folders[category].id;
+
+            // Import Flags
+            item.system.importFlags.name = foundry.utils.deepClone(item.name); // original english name for matching to icons
+            item.system.importFlags.type = item.type;
+            item.system.importFlags.subType = '';
+            item.system.importFlags.isFreshImport = true;
+
+            let subType = category.trim().split(' ').join('-');
+            if (SR5.itemSubTypes.quality.includes(subType)) {
+                item.system.importFlags.subType = subType;
+            }
+
+            // Default icon
+            item.img = await this.iconAssign(item.system.importFlags, item.system);
+
+            // Translate the name
             item.name = ImportHelper.MapNameToTranslation(this.itemTranslations, item.name);
 
             Helpers.injectActionTestsIntoChangeData(item.type, item, item);
-
-            // TODO: Move this to a more general base class
-            item.img = this.iconAssign(item.type, item.name, item.system);
 
             items.push(item);
         }
