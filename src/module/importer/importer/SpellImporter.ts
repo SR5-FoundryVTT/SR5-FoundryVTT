25c63cb82eeb0953aad431f1e80b648ebfb8ec6d
ian612 <34078802+ian612@users.noreply.github.com>
2023-09-28 20:30:06 -0400

    spell importer

--- src/module/importer/importer/SpellImporter.ts
+++ src/module/importer/importer/SpellImporter.ts
@@ -8,6 +8,7 @@ import { DetectionSpellImporter } from '../parser/spell/DetectionSpellImporter';
 import { ParserMap } from '../parser/ParserMap';
 import {Helpers} from "../../helpers";
 import { DataDefaults } from '../../data/DataDefaults';
+import { SR5 } from "../../config";
 
 export class SpellImporter extends DataImporter<Shadowrun.SpellItemData, Shadowrun.SpellData> {
     public override categoryTranslations: any;
@@ -50,19 +51,40 @@ export class SpellImporter extends DataImporter<Shadowrun.SpellItemData, Shadowr
         let jsonDatas = jsonObject['spells']['spell'];
         for (let i = 0; i < jsonDatas.length; i++) {
             let jsonData = jsonDatas[i];
+
+            // Check to ensure the data entry is supported
             if (DataImporter.unsupportedEntry(jsonData)) {
                 continue;
             }
 
+            // Create the item
             let item = parser.Parse(jsonData, this.GetDefaultData({type: 'spell'}), this.itemTranslations);
-            
             //@ts-ignore TODO: Foundry Where is my foundry base data?
             item.folder = folders[item.system.category].id;
 
-            Helpers.injectActionTestsIntoChangeData(item.type, item, item);
+            // Import Flags
+            item.system.importFlags.name = foundry.utils.deepClone(item.name); // original english name for matching to icons
+            item.system.importFlags.type = item.type;
+            item.system.importFlags.subType = '';
+            item.system.importFlags.isFreshImport = true;
 
-            // TODO: Move this to a more general base class
-            item.img = this.iconAssign(item.type, item.name, item.system);
+            // Add the subtype so the importer can add the correct icon
+            let subType = item.system.category;
+            if (SR5.itemSubTypes.spell.includes(subType)) {
+                item.system.importFlags.subType = subType;
+            }
+            else {
+                let x = 0;
+            }
+
+            // Default icon
+            item.img = await this.iconAssign(item.system.importFlags, item.system);
+
+            // Translate name if needed
+            item.name = ImportHelper.MapNameToTranslation(this.itemTranslations, item.name);
+
+            // Add relevant action tests
+            Helpers.injectActionTestsIntoChangeData(item.type, item, item);
 
             items.push(item);
         }
