201c88474c2d69e21a5c2ebc41c1c4c63ecea503
Jan Schoska <jan.schoska@gmail.com>
2023-10-14 13:01:27 +0200

    Active Effects Default Change Mode 'MODIFY' Fixes #746

--- src/module/effect/SR5ActiveEffectConfig.ts
+++ src/module/effect/SR5ActiveEffectConfig.ts
@@ -8,4 +8,24 @@ export class SR5ActiveEffectConfig extends ActiveEffectConfig {
         data.modes = {...data.modes, 0: game.i18n.localize('SR5.ActiveEffect.Modes.Modify')};
         return data;
     }
+
+    override activateListeners(html: JQuery<HTMLElement>): void {
+        super.activateListeners(html);
+    }
+
+    /**
+   * Handle adding a new change to the changes array.
+   * 
+   * This overrides the Foundry default behavior of using ADD as default.
+   * Shadowrun mostly uses MODIFY, so we use that as default.
+   * 
+   * @private
+   */
+  override async _addEffectChange(): Promise<this> {
+    //@ts-ignore TODO: foundry-vtt-types v10
+    const idx = this.document.changes.length;
+    return this.submit({preventClose: true, updateData: {
+      [`changes.${idx}`]: {key: "", mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM, value: ""}
+    }}) as unknown as this;
+  }
 }
