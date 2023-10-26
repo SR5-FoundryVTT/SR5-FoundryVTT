2db50dc19388d286bc26033f1fc10ebf627a0dee
BASIC-BIT <basic.bit.1001@gmail.com>
2023-10-26 14:35:18 -0400

    Fix a few references to translations that have moved

--- src/module/utils/strings.ts
+++ src/module/utils/strings.ts
@@ -1,5 +1,5 @@
 import * as englishTranslations from "../../../public/locale/en/config.json";
 import { NestedKeys } from './types';
 
-// Type encompassing all translated strings, flattening nested objects into only their leaf nodes (ie. "SR5.AttackHits", "SR5.Tooltips.Actor.MissingSummoner", etc.)
+// Type encompassing all translated strings, flattening nested objects into only their leaf nodes (ie. "SR5.Ammo", "SR5.TestResults.AttackHits", etc.)
 export type Translation = NestedKeys<typeof englishTranslations>;
