a8bd7008020159290dfd7929474acb76698d249d
Stexinator <hurricane90@gmx.de>
2023-10-26 08:25:47 +0200

    fix import for exotic_ranged

--- src/module/apps/characterImport/CharacterInfoUpdater.js
+++ src/module/apps/characterImport/CharacterInfoUpdater.js
@@ -255,6 +255,10 @@ export class CharacterInfoUpdater {
             if (name.includes('exotic') && name.includes('_weapon')) {
                 name = name.replace('_weapon', '');
             }
+
+            if (name.includes('exotic') && name.includes('_ranged')) {
+                name = name.replace('_ranged', '_range');
+            }
                
             if (name === 'pilot_watercraft') {
                 name = 'pilot_water_craft';
