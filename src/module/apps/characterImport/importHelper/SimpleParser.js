6d4d2735cae8de87bef05c6f9cf019cf518f6dd4
ian612 <34078802+ian612@users.noreply.github.com>
2023-10-26 23:12:20 -0400

    add icon assignment, set up actor and item sheets for echo & metamagic

--- src/module/apps/characterImport/importHelper/SimpleParser.js
+++ src/module/apps/characterImport/importHelper/SimpleParser.js
@@ -1,12 +1,18 @@
-import { parseDescription, createItemData } from "./BaseParserFunctions";
+import { parseDescription, createItemData, genImportFlags, formatAsSlug } from "./BaseParserFunctions";
+import * as IconAssign from '../../iconAssigner/iconAssign.js';
 
 export default class SimpleParser {
-    async parseCollection(parsingCollection, parserType) {
+    async parseCollection(parsingCollection, parserType, assignIcons) {
         const parsed = [];
+        const iconList = await IconAssign.getIconFiles();
 
         parsingCollection.forEach(async (toParse) => {
             try {
                 const itemData = this.parseItem(toParse, parserType);
+
+                // Assign the icon if enabled
+                if (assignIcons) {itemData.img = await IconAssign.iconAssign(itemData.system.importFlags, itemData.system, iconList)};
+
                 parsed.push(itemData);
             } catch (e) {
                 console.error(e);
@@ -19,6 +25,9 @@ export default class SimpleParser {
     parseItem(toParse, parserType) {
         const system = {};
         system.description = parseDescription(toParse);
+
+        // Assign import flags
+        system.importFlags = genImportFlags(formatAsSlug(toParse.name_english), parserType);
 
         return createItemData(toParse.fullname, parserType, system);
     }
---
+++ src/module/types/item/Echo.ts
@@ -0,0 +1,11 @@
+declare namespace Shadowrun {
+    export interface EchoData extends
+        EchoPartData,
+        ImportFlags,
+        DescriptionPartData {
+
+    }
+
+    export interface EchoPartData {
+    }
+}
