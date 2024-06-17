d0cb056b31a9d1b559d0bde8431f8475e273f389
taMiF <jan.schoska@gmail.com>
2024-06-17 21:34:35 +0200

    Basic case testing for matrix device value resolution

--- src/unittests/sr5.TestValueResolution.spec.ts
+++ src/unittests/sr5.TestValueResolution.spec.ts
@@ -1,4 +1,9 @@
 import { QuenchBatchContext } from '@ethaks/fvtt-quench';
+import { SR5Item } from '../module/item/SR5Item';
+import { DataDefaults } from '../module/data/DataDefaults';
+import { TestCreator } from '../module/tests/TestCreator';
+import { SR5Actor } from '../module/actor/SR5Actor';
+import { SR5TestingDocuments } from './utils';
 
 /**
  * Tests handle value resolution based on test context and actor / device type.
@@ -8,19 +13,66 @@ import { QuenchBatchContext } from '@ethaks/fvtt-quench';
 export const shadowrunTestValueResolution = (context: QuenchBatchContext) => {
     const { describe, it, assert, before, after } = context;
 
+    let testActor;
+    let testItem;
+
+    before(async () => {
+        testActor = new SR5TestingDocuments(SR5Actor);
+        testItem = new SR5TestingDocuments(SR5Item);
+    })
+
+    after(async () => {
+        await testActor.teardown();
+        await testItem.teardown();
+    })
+
     /**
      * Matrix related value resolutions based on owner, PAN, WAN, and direct connections.
      */
     describe('Matrix Tests', () => {
-        it('Calculate matrix device without owner', () => {
-            assert.fail();
+        it('Calculate matrix device without owner', async () => {
+            // @ts-expect-error
+            const device = new SR5Item({type: 'device', name: 'test', system: {'technology.rating': 3}});
+
+            assert.equal(device.system.technology?.rating, 3);
+            assert.equal(device.system.attributes?.willpower.value, 3);
+            assert.equal(device.system.attributes?.firewall.value, 3);
+
+            const action = DataDefaults.actionRollData({ attribute: 'willpower', attribute2: 'firewall' });
+            const test = await TestCreator.fromAction(action, device);
+
+            assert.equal(test?.pool.value, 6);
         });
 
-        it('Calculate matrix device with owner', () => {
-            assert.fail();
+        it('Calculate matrix device with owner', async () => {
+            const owner = await testActor.create({type: 'character', system: {'attributes.willpower.base': 5}});
+            const devices = await owner.createEmbeddedDocuments('Item', [{type: 'device', name: 'test', system: {'technology.rating': 3}}]);
+            const device = devices[0];
+
+            assert.equal(device.system.technology?.rating, 3);
+            assert.equal(device.system.attributes?.willpower.value, 3);
+            assert.equal(device.system.attributes?.firewall.value, 3);
+
+            assert.equal(owner.system.attributes?.willpower.value, 5);
+
+            const action = DataDefaults.actionRollData({ attribute: 'willpower', attribute2: 'firewall' });
+            const test = await TestCreator.fromAction(action, device);
+
+            assert.equal(test?.pool.value, 8);
         });
-        it('Calculate matrix device inside a PAN', () => {
-            assert.fail();
+        it('Calculate matrix device inside a PAN', async () => {
+            const master = await testItem.create({type: 'device', system: {'technology.rating': 5, 'category': 'commlink'}}) as SR5Item;
+            const slave = await testItem.create({type: 'equipment', system: {'technology.rating': 3}}) as SR5Item;
+
+            assert.equal(master.system.technology?.rating, 5);
+            assert.equal(slave.system.technology?.rating, 3);
+
+            await master.addNetworkDevice(slave);
+
+            const action = DataDefaults.actionRollData({ attribute: 'willpower', attribute2: 'firewall' });
+            const test = await TestCreator.fromAction(action, slave);
+
+            assert.equal(test?.pool.value, 8);
         });
 
         it('Calculate matrix device with owner and a direct connection', () => {
