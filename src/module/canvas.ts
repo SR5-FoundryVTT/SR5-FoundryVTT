1e82d3dfc01200d7169947d47c93b7629f0b34c4
taMiF <jan.schoska@gmail.com>
2024-08-17 11:34:10 +0200

    [BUG] Drag Ruler locks an actor in place on a square grid (#1277)
    * [BUG] Drag Ruler locks an actor in place on a square grid
    Fixes #1269
    
    tested for both v11 and v12

--- src/module/canvas.ts
+++ src/module/canvas.ts
@@ -4,13 +4,19 @@ interface DistanceOptions {
     gridSpaces?: boolean
 }
 
-// directly pulled from DND5e, just changed the
+/**
+ * Pulled from the FoundryVTT DND5e system: grid.mjs#measureDistance
+ * 
+ * @param segments 
+ * @param options 
+ * @returns 
+ */
 const measureDistances = function (segments, options: DistanceOptions = {}) {
     if (!options.gridSpaces) return BaseGrid.prototype.measureDistances.call(this, segments, options);
 
     // Track the total number of diagonals
     let nDiagonal = 0;
-    const rule = this.parent.diagonalRule;
+    const rule = this.diagonalRule;
     const d = canvas.dimensions as Canvas.Dimensions;
 
     // Iterate over measured segments
@@ -30,25 +36,26 @@ const measureDistances = function (segments, options: DistanceOptions = {}) {
         if (rule === '1-2-1') {
             let nd10 = Math.floor(nDiagonal / 2) - Math.floor((nDiagonal - nd) / 2);
             let spaces = nd10 * 2 + (nd - nd10) + ns;
-            // @ts-expect-error
+            // @ts-expect-error TODO: foundry-vtt-types v10
             return spaces * canvas.dimensions.distance;
         }
 
         // Treat diagonal as straight line
         else if (rule === 'EUCL') {
-            // @ts-expect-error
-            return Math.round(Math.hypot(nx, ny) * canvas.scene.data.gridDistance);
+            // @ts-expect-error TODO: foundry-vtt-types v10
+            // return Math.round(Math.hypot(nx, ny) * canvas.scene.data.gridDistance);
+            return Math.round(Math.hypot(nx, ny) * canvas.scene?.grid.distance);
         }
 
         // Treat diagonal as straight movement
-        // @ts-expect-error
-        else return (ns + nd) * canvas.scene.data.gridDistance;
+        // @ts-expect-error TODO: foundry-vtt-types v10
+        else return (ns + nd) * canvas.scene?.grid.distance;
     });
 };
 
 
 export function canvasInit() {
-    //@ts-expect-error
+    //@ts-expect-error TODO: foundry-vtt-types v10
     // Copy DnD5e's approach to movement measurement and add a custom field to the grid to be used in canvas.ts#measureDistances
     canvas.grid.diagonalRule = game.settings.get(SYSTEM_NAME, FLAGS.DiagonalMovement);
     // Add a custom measureDistances function, overwriting default to add more movement styles.
