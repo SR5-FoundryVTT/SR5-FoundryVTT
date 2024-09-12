d59d4a55891c90adb7ef25bafd405890bc154b01
taMiF <jan.schoska@gmail.com>
2024-09-12 20:34:18 +0200

    Resolved performance and freezing issues

--- src/module/utils/links.ts
+++ src/module/utils/links.ts
@@ -14,9 +14,7 @@ export class LinksHelpers {
      */
     static isURL(candidate: string | undefined): boolean {
         if (!candidate) return false;
-        var urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)?(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
-        var url = new RegExp(urlRegex, 'i');
-        return url.test(candidate);
+        return candidate.startsWith('http') || candidate.startsWith('https');
     }
 
     static async checkUuid(source: string | undefined): Promise<{document: any, resolvedUuid: any, anchor: string | undefined} | undefined> {
