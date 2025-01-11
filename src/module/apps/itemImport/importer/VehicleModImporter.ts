e000b5369cd8ede41cae3f12e3aa74d4a49febf7
Kévin Bergue <kevin@be-ta.art>
2025-01-11 11:36:48 +0100

    [Bug] Remove uneeded vehicule modifications categories and fix a bug in the vehicle importer (#1356)
    * [Rules] Remove uneeded vehicule modifications categories
    
    * [Bug] Switch around the folder naming convention in the vehicle importer

--- src/module/apps/itemImport/importer/VehicleModImporter.ts
+++ src/module/apps/itemImport/importer/VehicleModImporter.ts
@@ -46,9 +46,7 @@ export class VehicleModImporter extends DataImporter<Shadowrun.ModificationItemD
 
             // Get the item's folder information
             const folderName = categoryName === undefined         ? "" :
-                               categoryName === "All"             ? "Exotic" :
-                               categoryName === "Model-Specific"  ? "Exotic" :
-                               enhancement.includes(categoryName) ? "Other"  : categoryName;
+                               enhancement.includes(categoryName) ? categoryName : "Other";
 
             const folder = await ImportHelper.GetFolderAtPath("Item", `${Constants.ROOT_IMPORT_FOLDER_NAME}/Vehicle-Mods/${folderName}`, true);
             //@ts-expect-error TODO: Foundry Where is my foundry base data?
