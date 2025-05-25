212da37d00d51dce3bc71314e963b3f515b11890
Bruno Melo <bmc4@cin.ufpe.br>
2025-05-25 00:14:09 -0300

    Drone Body

--- src/module/apps/itemImport/parser/vehicle/VehicleParser.ts
+++ src/module/apps/itemImport/parser/vehicle/VehicleParser.ts
@@ -70,6 +70,7 @@ export class VehicleParser extends Parser<VehicleActorData> {
         system.vehicle_stats.acceleration.base = Number(jsonData.accel._TEXT) || 0;
         system.vehicle_stats.sensor.base = Number(jsonData.sensor._TEXT) || 0;
         system.vehicle_stats.seats.base = Number(jsonData.seats?._TEXT) || 0;
+        system.attributes.body.base = Number(jsonData.body._TEXT) || 0;
         system.armor.base = Number(jsonData.armor._TEXT) || 0;
         system.isDrone = jsonData.category._TEXT.includes("Drone") || false;
 
