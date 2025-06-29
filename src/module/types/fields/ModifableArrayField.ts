f2eb2145b34919466ac5733c18cb75548a85fac3
Bruno Melo <bmc4@cin.ufpe.br>
2025-06-29 15:44:52 -0300

    fix error introduced in ModifiableArrayfield

--- src/module/types/fields/ModifableArrayField.ts
+++ src/module/types/fields/ModifableArrayField.ts
@@ -15,21 +15,14 @@ import { SR5ActiveEffect } from "src/module/effect/SR5ActiveEffect";
  */
 export class ModifiableArrayField<
     const ElementFieldType extends DataField.Any,
-    const Options extends ArrayField.AnyOptions = ArrayField.DefaultOptions<
-        ArrayField.AssignmentElementType<ElementFieldType>
-    >,
+    const Options extends ArrayField.AnyOptions = ArrayField.DefaultOptions,
     const AssignmentElementType = ArrayField.AssignmentElementType<ElementFieldType>,
     const InitializedElementType = ArrayField.InitializedElementType<ElementFieldType>,
     const AssignmentType = ArrayField.AssignmentType<AssignmentElementType, Options>,
-    const InitializedType = ArrayField.InitializedType<AssignmentElementType, InitializedElementType, Options>,
+    const InitializedType = ArrayField.InitializedType<InitializedElementType, Options>,
     const PersistedElementType = ArrayField.PersistedElementType<ElementFieldType>,
-    const PersistedType extends PersistedElementType[] | null | undefined = ArrayField.PersistedType<
-        AssignmentElementType,
-        PersistedElementType,
-        Options
-    >
->
-    extends foundry.data.fields.ArrayField<ElementFieldType, Options, AssignmentElementType, InitializedElementType, AssignmentType, InitializedType, PersistedElementType, PersistedType> {
+    const PersistedType extends PersistedElementType[] | null | undefined = ArrayField.PersistedType<PersistedElementType, Options>
+> extends foundry.data.fields.ArrayField<ElementFieldType, Options, AssignmentElementType, InitializedElementType, AssignmentType, InitializedType, PersistedElementType, PersistedType> {
 
     override _applyChangeCustom(value: InitializedType, delta: InitializedType, model: DataModel.Any, change: ActiveEffect.ChangeData) {
         if (SR5ActiveEffect.applyModifyToModifiableValue(change.effect, model, change, value, delta)) return undefined;
