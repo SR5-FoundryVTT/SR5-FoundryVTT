f09397f7eadcefce1a2bb8a49bc981746902f2c6
MatyeusM <matthias.mandelartz@googlemail.com>
2025-07-13 19:19:04 +0200

    Fix tours deprecation

--- src/module/tours/sr5Tours.ts
+++ src/module/tours/sr5Tours.ts
@@ -1,12 +1,11 @@
-import { SR5Actor } from "../actor/SR5Actor";
+import { SR5Actor } from '../actor/SR5Actor';
 
-// @ts-expect-error
-export default class Sr5Tour extends Tour {
+// @ts-expect-error fall back for old versions, tour is any, which is pretty bad
+const tour = foundry?.nue?.Tour ?? Tour;
+export default class Sr5Tour extends tour {
     //the type of actor that should be created for the tour
-    actorType: String;
-
-    //the tab for the tour
-    tab?: String
+    actorType: string;
+    tab?: string;
 
     //this field is only for internal handling
     actor?: SR5Actor;
@@ -16,31 +15,28 @@ export default class Sr5Tour extends Tour {
         await super._preStep();
 
         //create actor if needed
-        if(this.actor == undefined) {
+        if (!this.actor) {
             this.actor = new SR5Actor.implementation({
-                //@ts-expect-error
-                name: "Tour " + this.id,
-                // @ts-expect-error
+                name: 'Tour ' + this.id,
                 type: this.config.actorType,
                 ownership: {
-                    default: 3
-                }
+                    default: 3,
+                },
             }) as SR5Actor;
         }
 
-        // @ts-expect-error
-        await this.actor.sheet?._render(true, {editable: false});
+        // @ts-expect-error render problematic
+        await this.actor.sheet?._render(true, { editable: false });
 
-        // @ts-expect-error
-        if(this.config.tab) {
-            // @ts-expect-error
-            this.actor?.sheet?.activateTab(this.config.tab)
+        if (this.config.tab) {
+            // @ts-expect-error activateTab problematic
+            this.actor?.sheet?.activateTab(this.config.tab);
         }
     }
 
     /** @override */
     async complete() {
-        await this.actor?.sheet?.close()
-        return super.complete()
-      }
+        await this.actor?.sheet?.close();
+        return super.complete();
+    }
 }
--- src/module/tours/tours.ts
+++ src/module/tours/tours.ts
@@ -1,26 +1,22 @@
-import Sr5Tour from "./sr5Tours";
+import Sr5Tour from './sr5Tours';
 
 export default async function registerSR5Tours() {
-  try {
+    try {
+        // @ts-expect-error
+        game.tours.register(
+            'shadowrun5e',
+            'ConditionMonitor',
+            await Sr5Tour.fromJSON('/systems/shadowrun5e/dist/tours/ConditionMonitor.json'),
+        );
 
-     // @ts-expect-error
-    game.tours.register(
-      'shadowrun5e',
-      'ConditionMonitor',
-       // @ts-expect-error
-      await Sr5Tour.fromJSON('/systems/shadowrun5e/dist/tours/ConditionMonitor.json'),
-    );
-
-
-//      game.tours.register(
-//       'shadowrun5e',
-//       'CharacterImport',
-//        // @ts-expect-error
-//       await Sr5Tour.fromJSON('/systems/shadowrun5e/dist/tours/character-import.json'),
-//     );
-//
-
-  } catch (err) {
-    console.log(err);
-  }
+        //      game.tours.register(
+        //       'shadowrun5e',
+        //       'CharacterImport',
+        //        // @ts-expect-error
+        //       await Sr5Tour.fromJSON('/systems/shadowrun5e/dist/tours/character-import.json'),
+        //     );
+        //
+    } catch (err) {
+        console.log(err);
+    }
 }
