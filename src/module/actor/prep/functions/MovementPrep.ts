731d2793c5d73897bf2af0484179392af7fbb831
taMiF <jan.schoska@gmail.com>
2025-07-19 16:12:38 +0200

    [Code] Implemented movement values as ModifiableField

--- src/module/actor/prep/functions/MovementPrep.ts
+++ src/module/actor/prep/functions/MovementPrep.ts
@@ -1,3 +1,4 @@
+import { Helpers } from "@/module/helpers";
 import {PartsList} from "../../../parts/PartsList";
 
 export class MovementPrep {
@@ -6,8 +7,10 @@ export class MovementPrep {
 
         const movement = system.movement;
         // default movement: WALK = AGI * 2, RUN = AGI * 4
-        // TODO: taMiF use base and calcTotal
-        movement.walk.value = attributes.agility.value * (2 + Number(modifiers['walk'])) + new PartsList(movement.walk.mod).total;
-        movement.run.value = attributes.agility.value * (4 + Number(modifiers['run'])) + new PartsList(movement.run.mod).total;
+        movement.walk.base = attributes.agility.value * (2 + Number(modifiers['walk'])) + new PartsList(movement.walk.mod).total;
+        movement.run.base = attributes.agility.value * (4 + Number(modifiers['run'])) + new PartsList(movement.run.mod).total;
+
+        Helpers.calcTotal(movement.walk, {min: 0});
+        Helpers.calcTotal(movement.run, {min: 0});
     }
 }
