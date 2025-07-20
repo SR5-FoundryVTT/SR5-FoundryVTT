e938c6e3292671ea0a265d3d926911e3f8805901
Neal Terrell <neal.terrell@csulb.edu>
2025-07-20 10:30:16 -0700

    Inject rigger attributes and skills for vehicle rolls when jumped in.

--- src/module/actor/flows/ActorRollDataFlow.ts
+++ src/module/actor/flows/ActorRollDataFlow.ts
@@ -17,7 +17,7 @@ export const ActorRollDataFlow = {
      * @param options
      */
     getRollData: function(actor: SR5Actor, rollData: any, options: RollDataOptions) {
-        if (actor.isVehicle()) ActorRollDataFlow.injectAstralRollData(actor, rollData, options);
+        if (actor.isVehicle()) ActorRollDataFlow.injectVehicleRiggerRollData(actor, rollData, options);
         return rollData;
     },
 
@@ -26,7 +26,20 @@ export const ActorRollDataFlow = {
      * 
      * TODO: Hand over to a RiggingRules implementation.
      */
-    injectVehicleRiggerRollData: function(actor: SR5Actor, rollData: any, options: RollDataOptions) {   
+    injectVehicleRiggerRollData: function(actor: SR5Actor, rollData: any, options: RollDataOptions) {
+        const driver = actor.getVehicleDriver()
+        const vehicleSystem = actor.system as Shadowrun.VehicleData;
+        const vehicleAttributes = actor.system.attributes as Shadowrun.AttributesData;
+        const driverSkills = driver?.system.skills as Shadowrun.CharacterSkills;
+        const driverAttributes = driver?.system.attributes as Shadowrun.AttributesData;
+        
+        if (vehicleSystem.controlMode == "rigger") {
+            const injectAttributes = ['intuition', 'agility'];
+            ActorRollDataFlow._injectAttributes(injectAttributes, driverAttributes, rollData, { bigger: false });  
+
+            const injectSkills = ['perception', 'sneaking', 'gunnery']
+            ActorRollDataFlow._injectSkills(injectSkills, driverSkills, rollData, { bigger: false });  
+        }
     },
 
     /**
@@ -39,5 +52,55 @@ export const ActorRollDataFlow = {
      * @param options 
      */
     injectAstralRollData: function(actor: SR5Actor, rollData: any, options: RollDataOptions) {
-    }
+    },
+
+    /**
+     * Inject all attributes into testData that match the given attribute names list.
+     * 
+     * Also implements the 'use bigger value rule',if necessary.
+     * 
+     * @param names A list of attribute names to inject
+     * @param attributes The list of source attribute to pull from
+     * @param rollData The testData to inject attributes into
+     * @param options.bigger If true, the bigger value will be used, if false the source value will always be used.
+     */
+    _injectAttributes(names: string[], attributes: Shadowrun.AttributesData, rollData: Shadowrun.ShadowrunItemDataData, options: { bigger: boolean }) {
+        const targetAttributes = rollData.attributes as Shadowrun.AttributesData;
+        for (const name of names) {
+            const sourceAttribute = foundry.utils.duplicate(attributes[name]);
+            const targetAttribute = targetAttributes[name];
+
+            if (options.bigger) {
+                targetAttributes[name] = sourceAttribute.value > targetAttribute.value ? sourceAttribute : targetAttribute;
+            } else {
+                targetAttributes[name] = sourceAttribute;
+            }
+        }
+    },
+
+    /**
+     * Inject all attributes into testData that match the given attribute names list.
+     * 
+     * Also implements the 'use bigger value rule',if necessary.
+     * 
+     * @param names A list of attribute names to inject
+     * @param attributes The list of source attribute to pull from
+     * @param rollData The testData to inject attributes into
+     * @param options.bigger If true, the bigger value will be used, if false the source value will always be used.
+     */
+    _injectSkills(names: string[], skills: Shadowrun.CharacterSkills, rollData: Shadowrun.ShadowrunActorDataData, options: { bigger: boolean }) {
+        const targetAttributes = rollData.skills.active as Shadowrun.AttributesData;
+        for (const name of names) {
+            console.log(skills)
+            console.log(name)
+            const sourceAttribute = foundry.utils.duplicate(skills.active[name]);
+            const targetAttribute = targetAttributes[name];
+
+            if (options.bigger) {
+                targetAttributes[name] = sourceAttribute.value > targetAttribute.value ? sourceAttribute : targetAttribute;
+            } else {
+                targetAttributes[name] = sourceAttribute;
+            }
+        }
+    },
 }
