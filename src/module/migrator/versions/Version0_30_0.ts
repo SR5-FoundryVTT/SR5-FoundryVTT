716e744803c292a6e95e6b70942ae77632466645
taMiF <jan.schoska@gmail.com>
2025-08-09 20:36:23 +0200

    Broken. First small steps.

--- src/module/migrator/versions/Version0_30_0.ts
+++ src/module/migrator/versions/Version0_30_0.ts
@@ -3,9 +3,9 @@ import { VersionMigration } from "../VersionMigration";
 /**
  * Version 0.29.0
  * 
- * Migrate Active Model to Data Model.
+ * - Migrate template data to DataModel.
+ * - Migrate old matrix data to matrix 1.0
  */
-
 export class Version0_30_0 extends VersionMigration {
     readonly TargetVersion = "0.30.0";
 
@@ -13,6 +13,7 @@ export class Version0_30_0 extends VersionMigration {
     override handlesActor(_actor: Readonly<any>) { return true; }
 
     override migrateItem(item: any) {
+        // CLeanup from legacy / broken template data to DataModel.
         if (item.type === 'sin' && item.system?.licenses)
             item.system.licenses = Object.values(item.system.licenses);
 
@@ -21,6 +22,23 @@ export class Version0_30_0 extends VersionMigration {
 
             if (item.system?.mount_point === 'under_barrel')
                 item.system.mount_point = 'under';
+        }
+
+        // Matrix 1.0 changes wireless from a checkbox to a choice.
+        if (item.type === 'host') {
+            migrateHostData(item);
+        }
+        if (item.type === 'device') {
+            migrateDeviceData(item);
+        }
+        if (item.system.technology) {
+            migrateMatrixDeviceData(item);
+        }
+    }
+
+    override migrateActor(actor: any) {
+        if (actor.isType('ic')) {
+            migrateIcActor(actor);
         }
     }
 
@@ -59,10 +77,46 @@ export class Version0_30_0 extends VersionMigration {
 
         if (flag.selection_skills)
             effect.system.selection_skills = JSON.parse(flag.selection_skills);
-        
+
         if (flag.selection_tests)
             effect.system.selection_tests = JSON.parse(flag.selection_tests);
 
         delete effect.flags.shadowrun5e;
     }
+}
+
+function migrateHostData(item: any) {
+    const marksData = item.system.marks;
+    if (!marksData) return
+    if (Array.isArray(marksData)) return
+
+    // pre v30 hosts stored marks as an object. As functionality to add marks is long past disabled, we can just delete marks.
+    item.system.marks = [];
+}
+
+function migrateDeviceData(item: any) {
+    // TODO: remove system.networkDevices
+    // TODO: remove system.slaves
+    // TODO: if possible recreate slave relation
+}
+
+/**
+ * pre v30 wirelss was a simple boolean
+ */
+function migrateMatrixDeviceData(item: any) {
+    if (item.system.technology) {
+        item.system.technology.wireless = item.system.technology.wireless ? 'online' : 'offline';
+    }
+
+    // TODO: remove system.technology.networkController
+    // TODO: if possible recreate slave relation
+    // const uuid = item.system?.technology?.networkController ?? '';
+    // const controller = await fromUuid(uuid) as SR5Item;
+    // if (!controller) return updateData;
+
+    // await NetworkStorage.addSlave(controller, item);
+}
+
+function migrateIcActor(actor: any) {
+    // TODO: See item for addSlave issues.
 }
