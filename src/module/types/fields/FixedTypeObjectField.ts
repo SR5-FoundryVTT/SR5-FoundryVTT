48b6f6c672835ef3614b153f30f2cc8ded357015
Jan Schoska <jan.schoska@gmail.com>
2025-08-11 17:19:01 +0200

    Improved documentation

--- src/module/types/fields/FixedTypeObjectField.ts
+++ src/module/types/fields/FixedTypeObjectField.ts
@@ -13,11 +13,11 @@ export class FixedTypeObjectField<
     /**
      * Get the field at the specified path for nested object keys.
      * 
-     * This is fix for broken v13 behavior:
+     * This is a fix for broken v13 behavior:
      * - Solution taken from: https://github.com/foundryvtt/foundryvtt/issues/13286
      * - Issue: https://github.com/foundryvtt/foundryvtt/issues/13249
      * 
-     * TODO: foundry-vtt v14 should have fixed this. Remove by then.
+     * TODO: fvtt-vtt v14 should fix this. Remove by then.
      * 
      * @param path The path to the field.
      * @returns The field at the specified path.
@@ -25,9 +25,9 @@ export class FixedTypeObjectField<
     override _getField(path) {
         if (path.length === 0) return this;
         const key = path.shift();
-        // @ts-expect-error
+        // @ts-expect-error TODO: fvtt-types v14
         if ((key === this.element.name) || (this.validateKey?.(key) !== false)) {
-            // @ts-expect-error
+            // @ts-expect-error TODO: fvtt-types v13 getFields exists on DataField but it's not getting recognized
             return this.element.getField(path);
         }
     }
