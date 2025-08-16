c3dd870b173eb1e492945067705c6a5e19172dd6
taMiF <jan.schoska@gmail.com>
2025-08-16 15:34:58 +0200

    Improved docs for Foundry v13 hotfix

--- src/module/types/fields/FixedTypeObjectField.ts
+++ src/module/types/fields/FixedTypeObjectField.ts
@@ -18,14 +18,15 @@ export class FixedTypeObjectField<
      * - Issue: https://github.com/foundryvtt/foundryvtt/issues/13249
      * 
      * TODO: fvtt-vtt v14 should fix this. Remove by then.
+     * NOTE: This will either return a matching field or undefined.
      * 
      * @param path The path to the field.
-     * @returns The field at the specified path.
+     * @returns The field at the specified path or undefined
      */
     override _getField(path) {
         if (path.length === 0) return this;
         const key = path.shift();
-        // @ts-expect-error TODO: fvtt-types v14
+        // @ts-expect-error TODO: fvtt-types v14 validateKey defaults to foundry.packages.BasePackage.validateId, disallowing invalid keys NOT checking for nested object keys
         if ((key === this.element.name) || (this.validateKey?.(key) !== false)) {
             // @ts-expect-error TODO: fvtt-types v13 getFields exists on DataField but it's not getting recognized
             return this.element.getField(path);
