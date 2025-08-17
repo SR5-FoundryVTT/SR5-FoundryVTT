754abb5c4ffc11be29276741daec3aaab24d5fe7
Bruno Melo <48841672+BM123499@users.noreply.github.com>
2025-08-17 00:21:44 -0300

    Change Width to 560px (D&D used value)

--- src/module/effect/SR5ActiveEffectConfig.ts
+++ src/module/effect/SR5ActiveEffectConfig.ts
@@ -58,7 +58,7 @@ export class SR5ActiveEffectConfig extends foundry.applications.sheets.ActiveEff
             addChange: this.#onAddChange,
         },
         classes: ["active-effect-config", SR5_APPV2_CSS_CLASS],
-        position: { width: 580 },
+        position: { width: 560 },
     }
 
     static override TABS = {
