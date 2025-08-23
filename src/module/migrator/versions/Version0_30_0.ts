e6844fe6be6c9682ec87c4b804111093022a7b94
taMiF <jan.schoska@gmail.com>
2025-08-23 18:58:07 +0200

    Cleaned up matrix migration

--- src/module/migrator/versions/Version0_30_0.ts
+++ src/module/migrator/versions/Version0_30_0.ts
@@ -13,37 +13,31 @@ export class Version0_30_0 extends VersionMigration {
     override handlesActor(_actor: Readonly<any>) { return true; }
 
     override migrateActor(_actor: any): void {
-        if (_actor.isType('ic')) {
-            migrateIcActor(_actor);
+        // Matrix 1.0 changes marks placed.
+        if (['character', 'critter', 'ic', 'vehicle', 'sprite'].includes(_actor.type)) {
+            migrateMarksActor(_actor);
         }
     }
 
-    override migrateItem(item: any) {
+    override migrateItem(_item: any) {
         // CLeanup from legacy / broken template data to DataModel.
-        if (item.type === 'sin' && item.system?.licenses)
-            item.system.licenses = Object.values(item.system.licenses);
+        if (_item.type === 'sin' && _item.system?.licenses)
+            _item.system.licenses = Object.values(_item.system.licenses);
 
-        if (item.type === 'modification' && typeof item.system?.mount_point === 'string') {
-            item.system.mount_point = item.system.mount_point.toLowerCase();
+        if (_item.type === 'modification' && typeof _item.system?.mount_point === 'string') {
+            _item.system.mount_point = _item.system.mount_point.toLowerCase();
 
-            if (item.system?.mount_point === 'under_barrel')
-                item.system.mount_point = 'under';
+            if (_item.system?.mount_point === 'under_barrel')
+                _item.system.mount_point = 'under';
         }
 
+        // Matrix 1.0 changes marks placed.
+        if (_item.type === 'host') {
+            migrateHostData(_item);
+        }
         // Matrix 1.0 changes wireless from a checkbox to a choice.
-        if (item.type === 'host') {
-            migrateHostData(item);
-        }
-        if (item.type === 'device') {
-            migrateDeviceData(item);
-        }
-        if (item.system.technology) {
-            migrateMatrixDeviceData(item);
-        }
-
-        // migrate wireless from a boolean to a string
-        if (item.system?.technology?.wireless === true) {
-            item.system.technology.wireless = 'online';
+        if (_item.system.technology?.wireless) {
+            migrateMatrixDeviceData(_item);
         }
     }
 
@@ -99,29 +93,20 @@ function migrateHostData(item: any) {
     item.system.marks = [];
 }
 
-function migrateDeviceData(item: any) {
-    // TODO: remove system.networkDevices
-    // TODO: remove system.slaves
-    // TODO: if possible recreate slave relation
+function migrateMarksActor(actor: any) {
+    const marksData = actor.system.matrix.marks;
+    if (!marksData) return
+    if (Array.isArray(marksData)) return
+
+    // pre v30 characters stored marks as an object. As functionality to add marks is long past disabled, we can just delete marks.
+    actor.system.marks = [];
 }
+
 
 /**
  * pre v30 wirelss was a simple boolean
  */
 function migrateMatrixDeviceData(item: any) {
-    if (item.system.technology) {
-        item.system.technology.wireless = item.system.technology.wireless ? 'online' : 'offline';
-    }
-
-    // TODO: remove system.technology.networkController
-    // TODO: if possible recreate slave relation
-    // const uuid = item.system?.technology?.networkController ?? '';
-    // const controller = await fromUuid(uuid) as SR5Item;
-    // if (!controller) return updateData;
-
-    // await NetworkStorage.addSlave(controller, item);
+    item.system.technology.wireless = item.system.technology.wireless ? 'online' : 'offline';
 }
 
-function migrateIcActor(actor: any) {
-    // TODO: See item for addSlave issues.
-}
