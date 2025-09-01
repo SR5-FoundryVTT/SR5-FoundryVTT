5eb090b68602a9aadb4ba890df874d8323cc5205
Shawn Milligan <smilligan93@gmail.com>
2025-09-01 15:45:28 -0400

    filter out actions that require ownership when the target isn't owned

--- src/module/actor/SR5Actor.ts
+++ src/module/actor/SR5Actor.ts
@@ -2038,6 +2038,26 @@ export class SR5Actor<SubType extends Actor.ConfiguredSubType = Actor.Configured
     }
 
     /**
+     * Determine if this actor is the owner of the given UUID
+     * - this will check for items and for actors in the case of Vehicles/Drones
+     * @param uuid - uuid of the instance to check
+     */
+    async isOwnerOf(uuid: string): Promise<boolean> {
+        const device = await fromUuid(uuid) as any;
+        if (!device) return false;
+        if (device instanceof SR5Item) {
+            // check if we are the actorOwner of the item
+            return device.actorOwner === this;
+        } else if (device instanceof SR5Actor) {
+            if (device === this) return true;
+            // if it's a vehicle, check if we are the driver of it (this isn't perfect but will do for now)
+            const vehicle = device.asType('vehicle');
+            if (vehicle && vehicle.getVehicleDriver() === this) return true;
+        }
+        return false;
+    }
+
+    /**
      * Return the document used to store marks placed for this actor.
      *
      * For a normal character/technomancer this will be the actor itself, though for others this can differ.
