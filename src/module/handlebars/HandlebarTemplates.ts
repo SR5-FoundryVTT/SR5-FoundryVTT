9d708b9e53fb35a9baf725633b5d3099aa8c40bb
MatyeusM <matthias.mandelartz@googlemail.com>
2025-09-21 16:18:40 +0200

    Remove unsued template from being preloaded.

--- src/module/handlebars/HandlebarTemplates.ts
+++ src/module/handlebars/HandlebarTemplates.ts
@@ -105,7 +105,6 @@ export const preloadHandlebarsTemplates = async () => {
         'systems/shadowrun5e/dist/templates/actor-limited/parts/MiscVehicle.hbs',
         'systems/shadowrun5e/dist/templates/actor-limited/parts/MiscCritter.hbs',
 
-
         // item parts
         'systems/shadowrun5e/dist/templates/item/parts/description.hbs',
         'systems/shadowrun5e/dist/templates/item/parts/technology.hbs',
@@ -172,9 +171,6 @@ export const preloadHandlebarsTemplates = async () => {
         // Test chat messages and their parts
         'systems/shadowrun5e/dist/templates/rolls/success-test-message.hbs',
         'systems/shadowrun5e/dist/templates/rolls/parts/rolled-dice.hbs',
-
-        // Modifier management parts
-        'systems/shadowrun5e/dist/templates/apps/partials/modifiers-physical.hbs',
 
         // Application - Matrix Network Hacking
         'systems/shadowrun5e/dist/templates/apps/matrix-network-hacking/tabs/networks.hbs',
