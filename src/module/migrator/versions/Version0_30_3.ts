cfac8fe615a7d4312eb76411a10a5dbc6545fb05
Bruno Melo <48841672+BM123499@users.noreply.github.com>
2025-09-25 14:23:40 -0300

    Remove Version change

--- src/module/migrator/versions/Version0_30_3.ts
+++ src/module/migrator/versions/Version0_30_3.ts
@@ -1,15 +1,6 @@
 import { VersionMigration } from "../VersionMigration";
-import { SR5 } from "@/module/config";
 
 /**
- * Migration class for version 0.30.3 of the SR5-FoundryVTT system.
- * 
- * This migration addresses issues related to inventory data, specifically the incorrect default setting of `showAll` to `true`
- * for newly created inventories and the default 'carried' inventory since version 0.30.0. This could break the display of which
- * inventory an item belongs to.
- * 
- * The migration also ensures that selection values for attributes, categories, limits, skills, and tests in active effects
- * are properly set if missing, using the SR5 system data.
  */
 export class Version0_30_3 extends VersionMigration {
     readonly TargetVersion = "0.30.3";
