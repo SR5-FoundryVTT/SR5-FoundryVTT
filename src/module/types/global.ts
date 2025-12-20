2589f6a631fec8bd52ba42affea27c3905c99b45
Bruno Melo <48841672+BM123499@users.noreply.github.com>
2025-12-20 20:57:45 -0300

    revert parameter type change

--- src/module/types/global.ts
+++ src/module/types/global.ts
@@ -227,7 +227,7 @@ declare module "fvtt-types/configuration" {
             sr5_afterTestComplete: any;
             sr5_processTagifyElements: any;
             "routinglib.ready": () => void;
-            SR5_CastItemAction: (arg0: any) => void;
+            SR5_CastItemAction: (arg0: SR5Item) => void;
             SR5_PreActorItemRoll: (arg0: SR5Actor, arg1: SR5Item) => void;
             getSceneControlButtons: (arg0: any) => void;
             getCombatTrackerEntryContext: (arg0: any, arg1: any) => void;
