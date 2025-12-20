7a8754f43b2bd6b94437e890514663901c536ab0
Bruno Melo <48841672+BM123499@users.noreply.github.com>
2025-12-20 12:29:56 -0300

    update migration

--- src/module/migrator/versions/Version0_31_5.ts
+++ src/module/migrator/versions/Version0_31_5.ts
@@ -1,7 +1,8 @@
 import { VersionMigration } from "../VersionMigration";
 
 /**
- * Make sure that readonly fields are set to their correct values.
+ * Migration to update spirit actor attributes to use 'attributes.force' with 'base' and 'hidden' properties,
+ * moving the old 'system.force' value to 'attributes.force.base' and setting 'hidden' to false.
  */
 export class Version0_31_5 extends VersionMigration {
     readonly TargetVersion = "0.31.5";
@@ -11,7 +12,13 @@ export class Version0_31_5 extends VersionMigration {
     }
 
     override migrateActor(_actor: any) {
-        if (_actor.type === 'spirit' && _actor.system.attributes?.force !== _actor.system.force)
-            _actor.system.attributes.force.base = _actor.system.force || 6;
+        const force = _actor.system.attributes.force;
+
+        if (_actor.type === 'spirit' && force) {
+            force.base = _actor.system.force;
+            force.hidden = false;
+        }
+
+        delete _actor.system.force;
     }
 }
