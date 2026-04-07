80dce1f289519f776252dd0a6b11503e39967a3a
Bruno Melo <48841672+BM123499@users.noreply.github.com>
2026-04-07 10:59:12 -0300

    Revert "[Refactor] Removed systems manual roll implementation in favor of FoundryVTT standard manual roll (#1789)"
    This reverts commit ef7eae27562013c810caa8742a9507620634cf4f.

--- src/module/constants.ts
+++ src/module/constants.ts
@@ -47,6 +47,7 @@ export const FLAGS = {
     AutomateMultiDefenseModifier: 'AutomateMultiDefenseModifier',
     AutomateProgressiveRecoil: 'AutomateProgressiveRecoil',
     TurnsSinceLastAttack: 'turnsSinceLastAttack',
+    ManualRollOnSuccessTest: 'ManualRollOnSuccessTest',
     MarkImports: 'MarkImports',
     ImportIconFolder: 'ImportIconFolder',
     UseImportIconOverrides: 'UseImportIconOverrides',
