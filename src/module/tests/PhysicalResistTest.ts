6604ad2303ba6e522ebabbb49b1cfc28a376502b
Bruno Melo <48841672+BM123499@users.noreply.github.com>
2026-04-12 18:40:13 -0300

    Fix armor bug on damage resist test

--- src/module/tests/PhysicalResistTest.ts
+++ src/module/tests/PhysicalResistTest.ts
@@ -88,7 +88,7 @@ export class PhysicalResistTest extends SuccessTest<PhysicalResistTestData> {
         if (this.data.action.armor) {
             if (this.actor) {
                 const armor = this.actor.getArmor(this.data.incomingDamage);
-                ModifiableValue.addUniqueBase(this.data.pool, 'SR5.Armor', armor.value);
+                ModifiableValue.addUniqueBase(this.data.pool, 'SR5.Armor.label', armor.value);
             }
         }
     }
