dac3bd0dd61cb3e6dfd872a8749352549f798093
Bruno Melo <48841672+BM123499@users.noreply.github.com>
2026-05-06 20:14:58 -0300

    fix migrator

--- src/module/migrator/versions/Version0_34_0.ts
+++ src/module/migrator/versions/Version0_34_0.ts
@@ -1,7 +1,7 @@
 import { SR } from "@/module/constants";
 import { VersionMigration } from "../VersionMigration";
 
-const { hasProperty } = foundry.utils;
+const { hasProperty, setProperty, getProperty } = foundry.utils;
 
 /**
  * Migrate initiative to have a blitz field, and vehicle stats to have off-road acceleration.
@@ -15,8 +15,10 @@ export class Version0_34_0 extends VersionMigration {
         if (hasProperty(system as any, "initiative"))
             system.initiative.blitz = system.initiative.edge;
 
-        if (actor.type === "vehicle" && hasProperty(system as any, "vehicle_stats"))
-            system.vehicle_stats.off_road_acceleration.base = system.vehicle_stats.acceleration.base;
+        if (actor.type === "vehicle" && hasProperty(system as any, "vehicle_stats")) {
+            const acceleration = getProperty(system, "vehicle_stats.acceleration.base");
+            setProperty(system, "vehicle_stats.off_road_acceleration.base", acceleration);
+        }
     }
 
     override migrateCombat(combat: any) {
