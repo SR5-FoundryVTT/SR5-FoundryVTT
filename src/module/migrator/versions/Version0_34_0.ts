a94dc83fd53a5f7306a44a05cc68da3da36b3eb8
Bruno Melo <48841672+BM123499@users.noreply.github.com>
2026-05-06 17:46:38 -0300

    add migration

--- src/module/migrator/versions/Version0_34_0.ts
+++ src/module/migrator/versions/Version0_34_0.ts
@@ -1,19 +1,29 @@
 import { SR } from "@/module/constants";
 import { VersionMigration } from "../VersionMigration";
 
+const { hasProperty } = foundry.utils;
+
 /**
- * Update attribute limits to their correct values.
+ * Migrate initiative to have a blitz field, and vehicle stats to have off-road acceleration.
+ * Also migrate combat flags for tracking initiative pass and if a combatant attacked last turn.
  */
 export class Version0_34_0 extends VersionMigration {
     readonly TargetVersion = "0.34.0";
 
     override migrateActor(actor: any): void {
-        actor.system.initiative.blitz = actor.system.initiative.edge;
+        const system = actor.system;
+        if (hasProperty(system as any, "initiative"))
+            system.initiative.blitz = system.initiative.edge;
+
+        if (actor.type === "vehicle" && hasProperty(system as any, "vehicle_stats"))
+            system.vehicle_stats.off_road_acceleration.base = system.vehicle_stats.acceleration.base;
     }
 
     override migrateCombat(combat: any) {
         const initiativePass = combat.flags.shadowrun5e?.combatInitiativePass ?? SR.combat.FIRST_PASS;
-        combat.system.pass = Math.max(initiativePass, SR.combat.FIRST_PASS);
+
+        if (hasProperty(combat, "system.pass"))
+            combat.system.pass = Math.max(initiativePass, SR.combat.FIRST_PASS);
     }
 
     override migrateCombatant(combatant: any): void {
